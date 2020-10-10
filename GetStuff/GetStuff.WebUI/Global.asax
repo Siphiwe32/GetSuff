432a1a6cb6cb12159e6c643a11c77ab996976696
Siphiwe Ngcobo <17598463@vcconnect.co.za>
2020-10-10 21:29:47 +0200

    Initial Solution Setup

---
+++ GetStuff/GetStuff.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GetStuff.WebUI.MvcApplication" Language="C#" %>
---
+++ GetStuff/GetStuff.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetStuff.WebUI.Startup))]
+namespace GetStuff.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
